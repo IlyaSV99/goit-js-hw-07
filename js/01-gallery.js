ca038277ba6e0753c574a1562b40ea8bf2b17e35
IlyaSV99 <ilavelicko81@gmail.com>
2022-05-27 15:53:58 +0300

    add hw files

---
+++ js/01-gallery.js
@@ -0,0 +1,45 @@
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector('.gallery');
+
+function renderGalleryMarkup(arr) {
+
+    const renderImgList = arr.map(({ description, preview, original}) => `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}" loading="lazy">
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/ loading="lazy"></a></div>`).join('\n');
+
+    
+    gallery.insertAdjacentHTML('beforeend', renderImgList);
+}
+renderGalleryMarkup(galleryItems);
+
+gallery.addEventListener('click', onModalOpen);
+
+function onModalOpen(e) {
+    e.preventDefault();
+    
+    const target = e.target;
+    if (target.nodeName !== 'IMG') {
+        return;
+    }
+
+    let urlImgOriginal = target.dataset.source;
+    
+   
+    const useLightBox = basicLightbox.create(`
+    <img src="${urlImgOriginal}" width="1280" height="auto">`, { onShow: () => {document.addEventListener('keydown', closeByEsc)},
+        onClose: () => {document.removeEventListener('keydown', closeByEsc)}}
+       );
+
+    useLightBox.show();
+    
+    function closeByEsc(e) {
+        if (e.code !== 'Escape') {
+            return;
+        }
+        useLightBox.close();
+    }
+    
+}
+
